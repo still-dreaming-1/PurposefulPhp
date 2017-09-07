349811b186b2b6b8a6118b49c1d4b6394b722040
still-dreaming-1 <jessedickey@gmail.com>
2017-09-07 10:04:48 -0600

    refactored

--- src/Examples/Dci/RolePlayerTest.php
+++ src/Examples/Dci/RolePlayerTest.php
@@ -5,33 +5,37 @@ namespace StillDreamingOne\PurposefulPhp\Examples\Dci;
 
 final class RolePlayerTest extends \PHPUnit\Framework\TestCase
 {
+    private $object;
+
+    public function setup()
+    {
+        $this->object = new RolePlayer();
+    }
+
     public function testInjectAndCall()
     {
-        $object = new RolePlayer();
         $injectedMethod = function() {
             return 5;
         };
-        $object->getFive = $injectedMethod;
-        $this->assertSame($object->getFive(), 5);
+        $this->object->getFive = $injectedMethod;
+        $this->assertSame($this->object->getFive(), 5);
     }
 
     public function testInjectAndCallWithOneParam()
     {
-        $object = new RolePlayer();
         $injectedMethod = function($add2To) {
             return $add2To + 2;
         };
-        $object->add2To = $injectedMethod;
-        $this->assertSame($object->add2To(1), 3);
+        $this->object->add2To = $injectedMethod;
+        $this->assertSame($this->object->add2To(1), 3);
     }
 
     public function testInjectAndCallWithTwoParams()
     {
-        $object = new RolePlayer();
         $injectedMethod = function($first, $second) {
             return $first + $second;
         };
-        $object->sum = $injectedMethod;
-        $this->assertSame($object->sum(2, 7), 9);
+        $this->object->sum = $injectedMethod;
+        $this->assertSame($this->object->sum(2, 7), 9);
     }
 }
