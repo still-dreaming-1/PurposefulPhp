34df8ef400189c7195ee686f756f71f35bd222b9
still-dreaming-1 <jessedickey@gmail.com>
2017-09-24 11:04:43 -0600

    Work in progress commit:
    fixed stuff and things

--- src/Condition.php
+++ src/Condition.php
@@ -5,14 +5,35 @@ namespace StillDreamingOne\PurposefulPhp;
 
 final class Condition
 {
+    /**
+     * @var When
+     */
+    private $when;
+    /**
+     * @var When
+     */
+    private $andThen;
+    /**
+     * @var Then
+     */
+    private $then;
+
     public function when(): When
     {
-        return new When();
+        $this->when = new When($this);
+        return $when;
+    }
+
+    public function andThen(): When
+    {
+        $this->andThen = new When($this);
+        return $this->andThen;
     }
 
     public function then(): Then
     {
-        return new Then();
+        $this->then = new Then();
+        return $this->then;
     }
 
     public function customerCalledWith(string $methodName, $nameArg, $preconditionAnyClosure): void
--- src/When.php
+++ src/When.php
@@ -6,10 +6,20 @@ namespace StillDreamingOne\PurposefulPhp;
 final class When
 {
     /**
+     * @var Condition
+     */
+    private $relationship;
+
+    public function __construct(Condition $relationship)
+    {
+        $this->relationship = $relationship;
+    }
+    /**
      * After $methodName, pass the parameters it is called with
      */
-    public function customerCallsWith(string $methodName): void
+    public function customerCallsWith(string $methodName): Condition
     {
         $arguments = \func_get_args();
+        return $this->relationship;
     }
 }
