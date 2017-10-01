bf61973a214427379f552cd68348179494cc00a5
still-dreaming-1 <jessedickey@gmail.com>
2017-10-01 10:02:47 -0600

    closer to working

--- src/ArgFilter.php
+++ src/ArgFilter.php
@@ -3,9 +3,15 @@ declare(strict_types=1);
 
 namespace StillDreamingOne\PurposefulPhp;
 
+use StillDreamingOne\PurposefulPhp\ArgTrap;
+
 final class ArgFilter
 {
+    /** @var ArgTrap */
+    private $argTrap;
+
     public function __construct(ArgTrap $argTrap)
     {
+        $this->argTrap = $argTrap;
     }
 }
